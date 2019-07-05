0887dc34338b484121fc6d9e5ad4644a17e3fea4
yoshida <yoshida.eth0@gmail.com>
2019-07-05 14:54:54 +0900

    mod Fx:Chorus feedback added

--- lib/audio_stream/fx/chorus.rb
+++ lib/audio_stream/fx/chorus.rb
@@ -3,11 +3,11 @@ module AudioStream
     class Chorus
       include BangProcess
 
-      def initialize(soundinfo)
+      def initialize(soundinfo, depth: 100, rate: 0.25)
         @soundinfo = soundinfo
 
-        @depth = 100
-        @rate = 0.25
+        @depth = depth
+        @rate = rate
 
         @delaybuf0 = RingBuffer.new(@depth * 3, 0.0)
         @delaybuf1 = RingBuffer.new(@depth * 3, 0.0)
@@ -21,17 +21,6 @@ module AudioStream
         channels = input.channels
 
         window_size.times {|i|
-          case channels
-          when 1
-            @delaybuf0.current = input[i]
-            @delaybuf0.rotate
-          when 2
-            @delaybuf0.current = input[i][0]
-            @delaybuf1.current = input[i][1]
-            @delaybuf0.rotate
-            @delaybuf1.rotate
-          end
-
           tau = @depth * (Math.sin(@speed * (@phase + i)) + 1)
           t = i - tau
 
@@ -46,6 +35,17 @@ module AudioStream
             wet0 = delta * @delaybuf0[i-m+1] + (1.0 - delta) * @delaybuf0[i-m]
             wet1 = delta * @delaybuf1[i-m+1] + (1.0 - delta) * @delaybuf1[i-m]
             input[i] = [(input[i][0] + wet0) * 0.5, (input[i][1] + wet1) * 0.5]
+          end
+
+          case channels
+          when 1
+            @delaybuf0.current = input[i]
+            @delaybuf0.rotate
+          when 2
+            @delaybuf0.current = input[i][0]
+            @delaybuf1.current = input[i][1]
+            @delaybuf0.rotate
+            @delaybuf1.rotate
           end
         }
         @phase = (@phase + window_size) % (window_size / @speed)
